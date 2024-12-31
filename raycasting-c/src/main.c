4df0424997d9777e929836718dde3f928bc81028
randycanegaly <canegaly@gmail.com>
2024-12-31 14:41:23 -0500

    Stopping. Guilt from neglecting K. Plus too much background noise.

--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+
+void initializeWindow(void);
+void processInput(void);
+void update(void);
+void render(void);
+void setup(void);
 
 int main(void) {
-    puts("Program is running.");
-    return 0;
+    //puts("Program is running.");
+    initializeWindow();
+
+	setup();
+	
+	while(1) {
+		processInput();
+		update();
+		render();
+	}
+
+	return 0;
 }
+
+void initializeWindow(void) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	
+}
+
+void processInput(void) {
+
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+
+}
+
+void setup(void) {
+
+}
+
